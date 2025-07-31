90826fb8502fc101ff7c4a4c3f0501a96e5c17b2
Syariful Hanif Setiawan <syarifulhanif@gmail.com>
2025-07-31 20:51:09 +0700

    fix initial state error handling in login component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import logo from "/public/images/logo-sibisa.png";  
 import Image from "next/image";
 
-const initialState = { error: null };
+const initialState = { error: "" };
 
 const Auth = () => {
   const [state, formAction] = useFormState(login, initialState);
